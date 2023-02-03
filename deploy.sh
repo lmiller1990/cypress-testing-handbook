9812e153d46013d30d1f947fc3ea64ec3d1ec755
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-02-03 17:11:54 +1000

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 git branch -D gh-pages
 git checkout -b gh-pages
 yarn docs:build
+mv docs/.vitepress/dist/ temp
 rm -rf docs
-mv docs/.vitepress/dist/ docs
+mv temp docs
 git add -A
 git commit -m "deploy"
 git push --set-upstream origin gh-pages -f
