23349a6f6ef8137888bb0029ceaaf5917b146a29
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-02-06 23:42:39 +1000

    sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,25 +1,25 @@
 export default {
-  title: 'Cypress Testing Handbook',
-  description: 'Just playing around.',
+  title: "Cypress Testing Handbook",
+  description: "Just playing around.",
   // base: '/cypress-testing-handbook/',
   themeConfig: {
     sidebar: [
       {
-        text: 'Basics',
+        text: "Basics",
         items: [
-          { text: 'Introduction', link: '/introduction' },
-          { text: 'Getting Started', link: '/getting-started' },
-          { text: 'Testing Props', link: '/testing-props' },
-          { text: 'Computed Properties', link: '/computed-properties' },
-        ]
+          { text: "Introduction", link: "/introduction" },
+          { text: "Getting Started", link: "/getting-started" },
+          { text: "Testing Props", link: "/testing-props" },
+          { text: "Computed Properties", link: "/computed-properties" },
+        ],
       },
       {
-        text: 'Integrating with Third Party Libraries',
+        text: "Integrating with Third Party Libraries",
         items: [
-          { text: 'Vuetify', link: '/vuetify' },
-        ]
+          { text: "Vuetify", link: "/vuetify" },
+          { text: "Pinia", link: "/pinia" },
+        ],
       },
-
-    ]
-  }
-}
+    ],
+  },
+};
